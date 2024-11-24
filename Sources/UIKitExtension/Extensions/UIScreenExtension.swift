89dfc018c4ca6e39f0c326289a72220b7a422f5d
Ivan Vorobei <hello@ivanvorobei.io>
2024-11-25 00:14:58 +0300

    Update UIScreenExtension.swift

--- Sources/UIKitExtension/Extensions/UIScreenExtension.swift
+++ Sources/UIKitExtension/Extensions/UIScreenExtension.swift
@@ -3,7 +3,7 @@ import UIKit
 public extension UIScreen {
     
     var displayCornerRadius: CGFloat {
-        guard let cornerRadius = self.value(forKey:"_displayCornerRadius") as? CGFloat else {
+        guard let cornerRadius = self.value(forKey: "_displayCornerRadius") as? CGFloat else {
             return .zero
         }
         return cornerRadius
